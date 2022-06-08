c92dc29ef80ce0610dc7454bd0f90e0317ac94f7
Justin Kian Romeon <92698759+justromeon@users.noreply.github.com>
2022-06-08 01:25:10 -0700

    Update e02.hs

--- ch04/e02.hs
+++ ch04/e02.hs
@@ -2,7 +2,7 @@
 --and splits its input list on every element for which the predicate returns False.
 
 splitWith :: (a -> Bool) -> [a] -> [[a]]
-slitWith _ [] = []
+splitWith _ [] = []
 splitWith f xs
     | null aList = splitWith f rest'
     | otherwise  = aList : splitWith f rest'
